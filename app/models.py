6937eae5a659291361729d95cfb03ed292e3bd04
BileichukIvan <godail2205@gmail.com>
2025-03-15 14:05:27 +0200

    Create User and Post models

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)
+    text = Column(Text)
