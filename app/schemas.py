dddbbbc8126447f05613fe0bf8a0abcf76c523bc
BileichukIvan <godail2205@gmail.com>
2025-03-15 14:07:20 +0200

    Define Pydantic schemas for data validation

---
+++ app/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, EmailStr, constr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: constr(min_length=6)
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class PostCreate(BaseModel):
+    text: constr(max_length=1048576)
