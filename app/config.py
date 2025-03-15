c754c29467711e4d46da89967639bbe74b06442e
BileichukIvan <godail2205@gmail.com>
2025-03-15 15:41:31 +0200

    Changed documentation for project in README.md file

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-CACHE_EXPIRY = int(os.getenv("CACHE_EXPIRY", 300))  # 5 хвилин
+CACHE_EXPIRY = int(os.getenv("CACHE_EXPIRY", 300))
